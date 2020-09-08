26c41335ad4f49b24469d1f76a61abf0c29be315
Carlos Estrada <carlos.estrada@gurudeviaje.com>
2020-09-08 18:16:04 -0500

    Cambio de jugador y estilos de X/O

--- funciones.js
+++ funciones.js
@@ -1,6 +1,20 @@
+// 1 - X    2 - O
+let turnoPrimerJugador = true
 
 function marcarCasilla () {
     let casilla = document.getElementById("casilla1");
-    casilla.classList.add("casilla-morada");
-    casilla.childNodes[0].innerText = 'X';
+
+    // Preguntamos de quien es el turno
+    if(turnoPrimerJugador) {
+        casilla.classList.add("casilla-morada");
+        casilla.classList.remove("casilla-azul")
+        casilla.childNodes[0].innerText = 'X';
+        turnoPrimerJugador = false
+    }
+    else {
+        casilla.classList.add("casilla-azul");
+        casilla.classList.remove("casilla-morada");
+        casilla.childNodes[0].innerText = 'O';
+        turnoPrimerJugador = true
+    }
 }
